5e006a6f75692bdc4c5a87063c61042416056079
andres0821 <acgilserna@gmail.com>
2018-05-22 23:36:23 -0500

    commit

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,6 @@
     <!--footer-->
     <footer class="my-5 pt-5 text-muted text-center text-small">
-        <p class="mb-1">© 2017-2018 Company Name </p>
+        <p class="mb-1">© 2017-2018 Company Name</p>
     </footer>
 
 </body>
